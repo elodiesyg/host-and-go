6c8e7bde3367499787a3a451b3495bf2523f77f1
ElodieSayavong <elodie.sayavong.dev@gmail.com>
2022-03-03 10:35:04 +0100

    firebase connection full

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,11 +1,9 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAryL3XrbyVqD4iq_8X5hqUy3iFRs567i4",
     authDomain: "host-and-go.firebaseapp.com",
@@ -17,8 +15,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+initializeApp(firebaseConfig);
+export default getFirestore();
